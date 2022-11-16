44fc5b9d3985f80dabd189544e58b6e69c1732bf
Arambarrix <63015432+Arambarrix@users.noreply.github.com>
2022-11-16 03:30:33 +0100

    First commit

---
+++ src/test/java/fr/istic/ticp/TicpApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.istic.ticp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
