ce6850319538d41761a5bf96134cccfa6254ad44
Tra-Lou Toure William <william.toure@etudiant.univ-rennes1.fr>
2022-11-16 02:58:32 +0000

    Revert "Merge branch 'master' into 'main'"
    This reverts merge request !4

--- src/main/java/fr/istic/ticp/model/Administrateur.java
+++
@@ -1,9 +0,0 @@
-package fr.istic.ticp.model;
-
-import java.time.LocalDate;
-import java.util.Objects;
-
-public class Administrateur extends Utilisateur{
-    private final String ROLE="administrateur";
-
-}
