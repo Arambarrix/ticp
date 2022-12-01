1b49979c94f7c8b16321477dbd8ce0656e8547a1
Brou Assamela <assamela.brou@etudiant.univ-rennes1.fr>
2022-12-01 14:43:10 +0100

    ConnexionView

---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+     <button class="bg-[#3A1900]" type="submit">Se connecter</button>
+</template>
+
+<style>
+button {
+    margin-left: 27%;
+    margin-top: 10%;
+  height: 45px;
+  width: 200px;
+  align-items: center;
+  color: white;
+  font-size: 24px;
+  border-radius: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+</style>
