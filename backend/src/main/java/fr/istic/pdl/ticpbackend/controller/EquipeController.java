241221d85abf29ff7e43b71cbad5c0c340f157ba
Arambarrix <63015432+Arambarrix@users.noreply.github.com>
2022-12-27 13:48:39 +0100

    Controller, Service et Repository des équipes (pull depuis le repo d'Alexandra)

--- backend/src/main/java/fr/istic/pdl/ticpbackend/controller/EquipeController.java
+++ backend/src/main/java/fr/istic/pdl/ticpbackend/controller/EquipeController.java
@@ -2,14 +2,38 @@ package fr.istic.pdl.ticpbackend.controller;
 
 import fr.istic.pdl.ticpbackend.service.EquipeService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import fr.istic.pdl.ticpbackend.model.Equipe;
+import java.util.*;
+
+
 
 @RestController
 @CrossOrigin("*")
 @AllArgsConstructor
 @RequestMapping("api/v1/equipes")
 public class EquipeController {
-    EquipeService service;
+    EquipeService equipeService;
+    // ecrire les methode ici
+
+    @GetMapping("/{id}")
+    private Optional<Equipe> getEquipe(@PathVariable("id") int id){
+        return equipeService.getEquipe(Long.valueOf(id));
+    }
+
+    @PostMapping("/")
+    private void saveEquipe(@RequestBody Equipe equipe){
+        equipeService.saveEquipe(equipe);
+    }
+    @PutMapping("/{id}")
+    private void updateEquipe(@RequestBody Equipe equipe, @PathVariable("id") int id){
+
+        equipeService.updateEquipe(equipe);
+    }
+
+    @DeleteMapping("/{id}")
+    private void deleteEquipe(@PathVariable("id") long id){
+        equipeService.deleteEquipe(id);
+    }
+
 }
