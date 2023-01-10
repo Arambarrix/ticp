70fddc1983af66ec3493d4a7a562d3d73e7f028f
abrou <assamela.brou@etudiant.univ-rennes1.fr>
2023-01-10 16:45:24 +0100

    Tableau implementation

--- frontend/src/components/TableauList.vue
+++ frontend/src/components/TableauList.vue
@@ -1,26 +1,8 @@
 <script setup>
     import MatchListTableVue from './tableaux/MatchList.vue';
 
-    import { computed } from "vue";
-    import { Tournois } from "@/stores/user/tournoi";
+    const props = defineProps({'tours':Array, can_edit:Boolean, show_cards:Boolean});
 
-    const props = defineProps({'tableaux':Array, can_edit:Boolean, show_cards:Boolean});
-
-    const tournoiStore = Tournois();
-
-    tournoiStore.getTournoiInfo()
-
-    const tournoi_tableaux_length = computed(()=>tournoiStore.getTableauxLength);
-    const tournoi_equipes_length = computed(()=>tournoiStore.getEquipesLength);
-    const tournoi_poules_length = computed(()=>tournoiStore.getPoulesLength);
-
-    var infoCardDatas= computed(()=>{
-        return {
-            "equipe":{'image':'user.png', 'number': tournoi_equipes_length.value, 'text':"Equipes Inscrites", 'color':"#1B2A5A"},
-            "poule":{'image':'group.png', 'number':tournoi_poules_length.value, 'text':"Poules Générées", 'color':"#195937"},
-            "tableau":{'image':'network.png', 'number':tournoi_tableaux_length.value, 'text':"Tableaux crées", 'color':"#00253A"}
-        } 
-    })
 
     
 </script>
