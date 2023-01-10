dcf1d9b248563f58df33cc9eb3bd18c941aba2b9
alexandra <apadonou>
2023-01-10 15:24:07 +0100

    suite du frontend admin

--- frontend/src/views/admin/Photo.vue
+++ frontend/src/views/admin/Photo.vue
@@ -31,7 +31,11 @@
         {
             id: 1,
             url:
+<<<<<<< HEAD
             'https://cdn.pixabay.com/photo/2019/07/21/22/32/bule-4353765_960_720.jpg',
+=======
+            'https://cdn.pixabay.com/photo/2021/11/11/13/08/leopard-6786267__340.jpg',
+>>>>>>> 320ba85 (suite du frontend admin)
         },
         {
             id: 2,
@@ -41,52 +45,107 @@
         {
             id: 3,
             url:
+<<<<<<< HEAD
             'https://cdn.pixabay.com/photo/2017/08/25/16/59/bowling-2680738_960_720.jpg',
+=======
+            'https://cdn.pixabay.com/photo/2020/09/23/17/12/flowers-5596564__340.png',
+>>>>>>> 320ba85 (suite du frontend admin)
         },
         {
             id: 1,
             url:
+<<<<<<< HEAD
                 'https://img.freepik.com/photos-premium/boules-petanque-dans-boite-herbe_524824-1528.jpg?w=740',
+=======
+                'https://cdn.pixabay.com/photo/2021/11/11/13/08/leopard-6786267__340.jpg',
+>>>>>>> 320ba85 (suite du frontend admin)
         },
         {
             id: 2,
             url:
+<<<<<<< HEAD
                 'https://img.freepik.com/photos-gratuite/amis-plein-coup-jouant-petanque-ensemble_23-2149530380.jpg?w=740&t=st=1673362515~exp=1673363115~hmac=137e71e1ac4111ddd9f84414d7700c6f25465a7a4f7d56f2fa661790de4dfc9b',
+=======
+                'https://cdn.pixabay.com/photo/2020/07/01/17/34/wolf-5360340__340.jpg',
+>>>>>>> 320ba85 (suite du frontend admin)
         },
         {
             id: 3,
             url:
+<<<<<<< HEAD
                 'https://img.freepik.com/photos-gratuite/hommes-faible-angle-jouant-plage_23-2149530353.jpg?w=740&t=st=1673362489~exp=1673363089~hmac=af2534f52df8c6fd2968253421d4c8ef1dbccb9f8f1ccc7a2f887e72ff5e66cb',
+=======
+                'https://cdn.pixabay.com/photo/2020/09/23/17/12/flowers-5596564__340.png',
+>>>>>>> 320ba85 (suite du frontend admin)
         },
         {
             id: 1,
             url:
+<<<<<<< HEAD
                 'https://img.freepik.com/photos-gratuite/vue-laterale-homme-lancant-balle_23-2149530335.jpg?t=st=1673362489~exp=1673363089~hmac=699581c2eb7c5363c74575849dfe26f1d62137dda61cefaae97fd874de085bfe',
+=======
+                'https://cdn.pixabay.com/photo/2021/11/11/13/08/leopard-6786267__340.jpg',
+>>>>>>> 320ba85 (suite du frontend admin)
         },
         {
             id: 2,
             url:
+<<<<<<< HEAD
                 'https://img.freepik.com/photos-gratuite/vue-laterale-amis-heureux-jouant-au-jeu-exterieur_23-2149530351.jpg?w=740&t=st=1673362838~exp=1673363438~hmac=cad899b090ae15c60fd9b4ef969f4f1707203557176134e0a8a7733a0f4435de',
+=======
+                'https://cdn.pixabay.com/photo/2020/07/01/17/34/wolf-5360340__340.jpg',
+>>>>>>> 320ba85 (suite du frontend admin)
         },
         {
             id: 3,
             url:
+<<<<<<< HEAD
                 'https://img.freepik.com/photos-gratuite/amis-ages-jouant-petanque_23-2149443926.jpg?w=740&t=st=1673362846~exp=1673363446~hmac=af4ff7e78db322d34b1abeb875394497c8d197c7f6f5915e8fd9daece0ca291a',
+=======
+                'https://cdn.pixabay.com/photo/2020/09/23/17/12/flowers-5596564__340.png',
+>>>>>>> 320ba85 (suite du frontend admin)
         },
         {
             id: 1,
             url:
+<<<<<<< HEAD
                 'https://img.freepik.com/photos-gratuite/amis-ages-jouant-petanque_23-2149443890.jpg?t=st=1673362846~exp=1673363446~hmac=81ed85eed8723954705726653e232af04db20fb377c57dce3cc95726c81b8710',
+=======
+                'https://cdn.pixabay.com/photo/2021/11/11/13/08/leopard-6786267__340.jpg',
+>>>>>>> 320ba85 (suite du frontend admin)
         },
         {
             id: 2,
             url:
+<<<<<<< HEAD
                 'https://img.freepik.com/photos-gratuite/vue-laterale-mains-tenant-boules-rouges-plage_23-2149530360.jpg?w=740&t=st=1673362808~exp=1673363408~hmac=048e1d8a7683da4a26c1bf4f204934ac3020bc5a33752157a0ade22870592446',
+=======
+                'https://cdn.pixabay.com/photo/2020/07/01/17/34/wolf-5360340__340.jpg',
+>>>>>>> 320ba85 (suite du frontend admin)
         },
         {
             id: 3,
             url:
+<<<<<<< HEAD
                 'https://img.freepik.com/photos-gratuite/amis-ages-jouant-petanque_23-2149443921.jpg?w=740&t=st=1673362835~exp=1673363435~hmac=0dd3037cf2e6d1a81d7a250e147b0bfaf8bcdec0d170c1980a6555f7da970310',
+=======
+                'https://cdn.pixabay.com/photo/2020/09/23/17/12/flowers-5596564__340.png',
+        },
+        {
+            id: 1,
+            url:
+                'https://cdn.pixabay.com/photo/2021/11/11/13/08/leopard-6786267__340.jpg',
+        },
+        {
+            id: 2,
+            url:
+                'https://cdn.pixabay.com/photo/2020/07/01/17/34/wolf-5360340__340.jpg',
+        },
+        {
+            id: 3,
+            url:
+                'https://cdn.pixabay.com/photo/2020/09/23/17/12/flowers-5596564__340.png',
+>>>>>>> 320ba85 (suite du frontend admin)
         }
     ]
 
